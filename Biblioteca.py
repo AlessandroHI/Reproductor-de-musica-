532dcf6c013e7fb6f8ba9c036e83b4ce640e9c33
AlessandroHI <3028601340106@ingenieria.usac.edu.gt>
2021-12-15 01:17:08 -0600

    Clase Biblioteca

---
+++ Biblioteca.py
@@ -0,0 +1,13 @@
+from typing import List
+from Lista import Lista
+
+class Biblioteca(Lista):
+
+    def __init__(self,artista,album,cancion,imagen,ruta):
+        self.artista = artista
+        self.album = album
+        self.cancion = cancion
+        self.imagen = imagen
+        self.ruta =ruta
+        super().__init__()
+        
