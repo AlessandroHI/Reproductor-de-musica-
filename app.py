859f9d623cabda132afa17256ec00d073957cb18
AlessandroHI <3028601340106@ingenieria.usac.edu.gt>
2021-12-16 18:59:55 -0600

    main

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from pygame import mixer
+
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
+from PIL import ImageTk, Image
 from Biblioteca import Biblioteca
 from Lista import Lista
 from xml.etree import ElementTree
@@ -21,11 +24,13 @@ def Buscar_archivo():  # FUNCION QUE REALIZAR LA SELLECION DEL ARCHIVO A CARGAR
     else:
         return archivo
 
+
+
 def cargarcanciones():
     archivo = Buscar_archivo()
     procesarCanciones(archivo)       
 
-def procesarCanciones(ruta): #Almacenando recorriendo y identificandio la informarcion
+def procesarCanciones(ruta): #Almacenando ,recorriendo e identificandio la informarcion
     global listaSu
     archivoXML = ElementTree.parse(ruta)
     root = archivoXML.getroot()
@@ -43,10 +48,10 @@ def procesarCanciones(ruta): #Almacenando recorriendo y identificandio la inform
           albu = album.text
       for imagen in cancion.iter("imagen"):
           #print("Imagen:",imagen.text.replace('"',""))
-          img = imagen.text
+          img = imagen.text.replace('"',"")
       for ruta in cancion.iter("ruta"):
           #print("Ruta:", ruta.text.replace('"',""))
-          ru = ruta.text
+          ru = ruta.text.replace('"',"")
           listaSu.append(Biblioteca(artist,albu,song,img,ru))
     canciones()
       
@@ -60,41 +65,56 @@ def canciones2():
     for i in range(listaSu.len()):    
         table3.insert("", END, text=str(listaSu[i].cancion),values=(str(listaSu[i].album).replace('"',""), str(listaSu[i].artista).replace('"',"")))
 
-def changeArtista(): #CAMBIOS DE ARTISTAS
-   listaOp =  []
-   for i in range(listaSu.len()):
-       listaOp.append(listaSu[i].artista.replace('"',""))
+def Play(ruta):
+    global mixer
+    mixer.init()
+    mixer.music.load(ruta)
+    mixer.music.set_volume(0.2)
+    mixer.music.play()
 
-   comboArtista['values'] = listaOp 
-   comboArtista1['values'] = listaOp 
+def Pause():
+    mixer.music.pause()
 
-def changeAlbum(): #CAMBIOS DE ALBUNES SEGUN ARTISTA
-   listaOp =  []
-   for i in range(listaSu.len()):
-       listaOp.append(listaSu[i].album.replace('"',""))
+def Reanudar():
+    mixer.music.unpause()
 
-   comboAlbum['values'] = listaOp
-   comboAlbum1['values'] = listaOp
+def get_data():
+    cancion = table1.item(table1.selection())["text"]
+    album = table1.item(table1.selection())["values"][0]
+    artista = table1.item(table1.selection())["values"][1]
+    label1['text'] = "Cancion: "+cancion
+    label2['text'] = "Artista: "+artista
+    label4['text'] = "Album: "+album
+    ruta = get_ruta(cancion)
+    imagen = get_img(cancion)
+    Play(ruta)
+    MostrarImagen(imagen)
 
-def changeSong(): #CAMBIONS DE CANCINES SEGUN ALBUM 
-   listaOp =  []
-   for i in range(listaSu.len()):
-       listaOp.append(listaSu[i].cancion.replace('"',""))
+def get_img(nombre):
+    for i in range(listaSu.len()):
+        if nombre == listaSu[i].cancion:
+            return listaSu[i].imagen
+        
 
-   comboSong['values'] = listaOp
-   comboSong1['values'] = listaOp
+def get_ruta(cancion):
+    for i in range(listaSu.len()):
+        if cancion == listaSu[i].cancion:
+            return listaSu[i].ruta
 
-def agregarsong():
-    artista = comboArtista1.get()
-    album = comboAlbum1.get()
-    cancion = comboSong1.get()
-    table4.insert("", END, text=cancion,values=(album,artista))
+def MostrarImagen(ruta):
+  img = Image.open(ruta)
+  new_img = img.resize((320,320))
+  render = ImageTk.PhotoImage(new_img)
+  img1 = Label(ventana, image=render)
+  img1.image = render
+  img1.place(x=30, y = 65)
 
-def datos():
-    artista = comboArtista.get()
-    album = comboAlbum.get()
-    cancion = comboSong.get()
-    print(artista,album,cancion)
+
+def addSong():
+    cancion = table3.item(table3.selection())["text"]
+    album = table3.item(table3.selection())["values"][0]
+    artista = table3.item(table3.selection())["values"][1]
+    table4.insert("", END, text=cancion,values=(album,artista ))
 
 
 
@@ -109,34 +129,9 @@ def crearLista():
 
     label12 = Label(ventanaLista,text="Nombre :")
     label12.place(x = 15,y = 60)
-
-    label8 = Label(ventanaLista,text="Cancion Agregar", width=25,height=1)
-    label8.config(fg="blue",bg="yellow", font=("Veranda",12))
-    label8.place(x = 20,y = 110)
         
-    label5 = Label(ventanaLista,text="Artista:")
-    label5.place(x = 50,y = 150)
-    global comboArtista1
-    comboArtista1 = ttk.Combobox(ventanaLista, height=1, width=18, values=['Aun no hay Artista'],postcommand=changeArtista ,state="readonly")
-    comboArtista1.current(0)
-    comboArtista1.place(x = 50,y =180)
-
-    label6 = Label(ventanaLista,text="Album:")
-    label6.place(x = 50,y = 220)
-    global comboAlbum1
-    comboAlbum1 = ttk.Combobox(ventanaLista, height=1, width=18, values=['Aun no hay Albunes'],postcommand=changeAlbum, state="readonly")
-    comboAlbum1.current(0)
-    comboAlbum1.place(x = 50,y =250)
-    
-    label7 = Label(ventanaLista,text="Canciones:")
-    label7.place(x = 50,y = 290)
-    global comboSong1
-    comboSong1 = ttk.Combobox(ventanaLista, height=1, width=20, values=['Aun no hay Canciones'],postcommand= changeSong, state="readonly")
-    comboSong1.current(0)
-    comboSong1.place(x = 50,y =320)
-
-    btnagregar = Button(ventanaLista, text="Agregar cancion", width=20,height=2, command=agregarsong)
-    btnagregar.place(x= 50, y=380)
+    btnagregar = Button(ventanaLista, text="Agregar cancion", width=20,height=2, command=addSong)
+    btnagregar.place(x= 50, y=120)
      
     btnMostrar = Button(ventanaLista, text="Mostrar Biblioteca", width=20,height=1,command=canciones2)
     btnMostrar.place(x= 400, y=10)
@@ -164,9 +159,9 @@ def crearLista():
     table4.place(x=300,y=320)
    
     btnsavelista = Button(ventanaLista, text="Guardar Lista", width=20,height=2)
-    btnsavelista.place(x= 400, y=570)
+    btnsavelista.place(x= 50, y=420)
 
-    ventanaLista.geometry('700x650+300+50')
+    ventanaLista.geometry('700x580+300+50')
     ventanaLista.mainloop()
 
 
@@ -177,87 +172,66 @@ if __name__ == '__main__':
 
     ventana = Tk()
     ventana.title("IPCmusic HOME")
-    
+
     btnCargar = Button(ventana, text="Cargar canciones", width=18,height=2, command=cargarcanciones)
     btnCargar.place(x= 10, y=10)
 
     btnLista = Button(ventana, text="Crear lista de reprodccion", width=20, height=2, command=  lambda: [crearLista()])
-    btnLista.place(x= 780, y=70)
+    btnLista.place(x= 820, y=5)
 
-    btnReportes = Button(ventana, text="Reportes", width=8, height=2, command=datos)
-    btnReportes.place(x= 1100, y=10)
+    btnReportes = Button(ventana, text="Reportes", width=8, height=2)
+    btnReportes.place(x= 160, y=10)
 
-    label1 = Label(ventana,text="Cancion: Pompeii")
+    play = Button(ventana, text="PLAY", width=8, height=2, command=get_data)
+    play.place(x= 20, y=400)
+
+    pause = Button(ventana, text="PAUSE", width=8, height=2, command=Pause)
+    pause.place(x= 90, y=400)
+
+    reanudar = Button(ventana, text="REANUDAR", width=9, height=2, command=Reanudar)
+    reanudar.place(x= 160, y=400)
+
+
+    label1 = Label(ventana,text="Cancion:")
     label1.place(x = 100,y = 460)
 
-    label2 = Label(ventana,text="Artista: Bastille")
+    label2 = Label(ventana,text="Artista:")
     label2.place(x = 100,y = 490)
 
-    label4 = Label(ventana,text="Album: Bastille")
+    label4 = Label(ventana,text="Album:")
     label4.place(x = 100,y = 520)
 #-----------------------------------------------------------------------------------
     label3 = Label(ventana,text="BIBLIOTECA", width=13,height=1)
     label3.config(fg="blue",bg="yellow", font=("Veranda",33))
     label3.place(x = 380,y = 13)
 
-    table1 = ttk.Treeview(ventana, columns=("Album","Artista") ,height=17)
+    table1 = ttk.Treeview(ventana, columns=("Album","Artista") ,height=26)
     table1.column("#0", width=115)
     table1.column("Album", width=115, anchor=CENTER)
     table1.column("Artista", width=115, anchor= CENTER)
     table1.heading("#0",text="CANCION", anchor=CENTER)
     table1.heading("Album",text="ALBUM", anchor=CENTER)
     table1.heading("Artista",text="ARTISTA", anchor=CENTER)
-    
-    label3 = Label(ventana,text="CANCION A REPRODUCIR", width=25,height=1)
-    label3.config(fg="blue",bg="yellow", font=("Veranda",15))
-    label3.place(x = 420,y = 450)
-        
-    label5 = Label(ventana,text="Artista:")
-    label5.place(x = 350,y = 495)
-    comboArtista = ttk.Combobox(ventana, height=1, width=18, values=['Aun no hay Artista'],postcommand=changeArtista ,state="readonly")
-    comboArtista.current(0)
-    comboArtista.place(x = 350,y =520)
-
-    label6 = Label(ventana,text="Album:")
-    label6.place(x = 490,y = 495)
-    comboAlbum = ttk.Combobox(ventana, height=1, width=18, values=['Aun no hay Albunes'],postcommand=changeAlbum, state="readonly")
-    comboAlbum.current(0)
-    comboAlbum.place(x = 490,y =520)
-    
-    label7 = Label(ventana,text="Canciones:")
-    label7.place(x = 630,y = 495)
-    comboSong = ttk.Combobox(ventana, height=1, width=20, values=['Aun no hay Canciones'],postcommand= changeSong, state="readonly")
-    comboSong.current(0)
-    comboSong.place(x = 630,y =520)
+         
     
     table1.place(x=380,y=65)
     #-------------------------------------------------------------------------------------
     label8 = Label(ventana,text="LISTAS DE REPRODUCCION", width=25,height=1)
     label8.config(fg="blue",bg="yellow", font=("Veranda",12))
-    label8.place(x = 930,y = 50)
+    label8.place(x = 780,y = 50)
 
     table2 = ttk.Treeview(ventana, columns=("Canciones") ,height=13)
     table2.column("#0", width=115)
     table2.column("Canciones", width=115, anchor=CENTER)
     table2.heading("#0",text="NOMBRE", anchor=CENTER)
     table2.heading("Canciones",text="No. CANCIONES", anchor=CENTER)
-    table2.place(x=930,y=70)
-
-    label9 = Label(ventana,text="LISTA A REPRODUCIR", width=25,height=1)
-    label9.config(fg="blue",bg="yellow", font=("Veranda",15))
-    label9.place(x = 930,y = 450)
-        
-    label10 = Label(ventana,text="Nombre Lista:")
-    label10.place(x = 940,y = 495)
-    comboName = ttk.Combobox(ventana, height=1, width=18, values=['Aun no hay Lista'],state="readonly")
-    comboName.current(0)
-    comboName.place(x = 940,y =520)
+    table2.place(x=780,y=70)
 
     label11 = Label(ventana,text="Modo:")
-    label11.place(x = 1090,y = 495)
+    label11.place(x = 820,y = 495)
     comboModo = ttk.Combobox(ventana, height=1, width=15, values=['---','Normal','Aleatorio'], state="readonly")
     comboModo.current(0)
-    comboModo.place(x = 1090,y =520)
+    comboModo.place(x = 820,y =520)
 
-    ventana.geometry('1250x660+150+40')
+    ventana.geometry('1050x660+200+40')
     ventana.mainloop()
